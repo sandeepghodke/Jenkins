00cc10708da5d73a4828821edc5eb8be35bd9c03
Sandeep, Ghodke <sandeep.ghodke@gmail.com>
2018-10-02 01:20:59 +0530

    deleted latest file

--- java-maven-junit-helloworld-master/src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
+++ java-maven-junit-helloworld-master/src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
@@ -9,7 +9,6 @@ public class HelloApp {
 	static int DEFAULT_TIMES = 3;
 	static int EXIT_STATUS_PARAMETER_NOT_UNDERSTOOD = 2;
 	static int EXIT_STATUS_HELLO_FAILED = 4;
-
 	/**
 	 * The main method of this program.
 	 *
